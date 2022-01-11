294ea5a25199da5465dd1fe3c239971d674224e5
redasalmi <reda.salmi.elt@gmail.com>
2022-01-11 18:46:03 +0100

    updated Links components types

--- app/components/Navbar/Links.tsx
+++ app/components/Navbar/Links.tsx
@@ -5,44 +5,54 @@ import GithubLink from '~/components/Navbar/GithubLink';
 import { NAV_LINKS } from '~/constant';
 
 interface LinksProps {
-  linksRef?: React.RefObject<HTMLDivElement>;
-  hideNavbar?: () => void;
-  mobile?: boolean;
+  mobile?: false;
 }
 
-export default function Links({
-  linksRef,
-  hideNavbar,
-  mobile = false,
-}: LinksProps) {
+interface MobileLinksProps {
+  linksRef: React.RefObject<HTMLDivElement>;
+  hideNavbar: () => void;
+  mobile: true;
+}
+
+export default function Links(props: LinksProps | MobileLinksProps) {
+  if (!props.mobile) {
+    return (
+      <div className="nav-links">
+        <ul className="nav-links-list">
+          {NAV_LINKS.map(({ href, text }) => (
+            <li key={href}>
+              <Link to={href} className="nav-link nav-animated-link">
+                {text}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+
+  const { linksRef, hideNavbar } = props;
+
   return (
-    <div ref={linksRef} className={mobile ? 'nav-mobile-links' : 'nav-links'}>
-      <ul className={mobile ? 'nav-mobile-links-list' : 'nav-links-list'}>
+    <div ref={linksRef} className="nav-mobile-links">
+      <ul className="nav-mobile-links-list">
         {NAV_LINKS.map(({ href, text }) => (
           <li key={href}>
-            <Link
-              to={href}
-              onClick={hideNavbar}
-              className={
-                mobile ? 'container nav-link' : 'nav-link nav-animated-link'
-              }
-            >
+            <Link to={href} onClick={hideNavbar} className="container nav-link">
               {text}
             </Link>
           </li>
         ))}
 
-        {mobile ? (
-          <li className="nav-github-link">
-            <GithubLink
-              onClick={hideNavbar}
-              linkClassName="nav-github-btn"
-              svgClassName="nav-github"
-            >
-              <span>Check out my github</span>
-            </GithubLink>
-          </li>
-        ) : null}
+        <li className="nav-github-link">
+          <GithubLink
+            onClick={hideNavbar}
+            linkClassName="nav-github-btn"
+            svgClassName="nav-github"
+          >
+            <span>Check out my github</span>
+          </GithubLink>
+        </li>
       </ul>
     </div>
   );
