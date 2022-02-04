b2da40b24121e5a2bf6f3fd7dd377e7c479ff328
redasalmi <reda.salmi.elt@gmail.com>
2022-02-05 00:01:53 +0100

    added favicon

--- app/constant/links.ts
+++ app/constant/links.ts
@@ -6,8 +6,22 @@ import extraLargeStyles from '~/styles/extra-large.css';
 
 export const links = [
   {
+    rel: 'manifest',
+    href: '/manifest.webmanifest',
+  },
+  {
     rel: 'icon',
     href: '/favicon.ico',
+    sizes: 'any',
+  },
+  {
+    rel: 'icon',
+    href: '/icon.svg',
+    type: 'image/svg+xml',
+  },
+  {
+    rel: 'apple-touch-icon',
+    href: '/apple-touch-icon.png',
   },
   {
     rel: 'stylesheet',
