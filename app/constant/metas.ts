1ea075910d38161881d37127c84391e1b7203a8e
redasalmi <reda.salmi.elt@gmail.com>
2022-02-07 12:20:04 +0100

    added seo meta tags

--- app/constant/metas.ts
+++ app/constant/metas.ts
@@ -1,3 +1,21 @@
+const title = 'Reda Salmi Portfolio';
+const description =
+  'Reda Salmi personal website with projects, skills and contact informations.';
+const twitterUser = '@redsalmi';
+
 export const metas = {
-  title: 'Reda Salmi Portfolio',
+  title,
+  description,
+
+  'twitter:card': 'summary',
+  'twitter:site': twitterUser,
+  'twitter:creator': twitterUser,
+
+  'og:title': title,
+  'og:description': description,
+  'og:type': 'website',
+  'og:url': 'https://redasalmi.netlify.app/',
+  'og:image': 'https://redasalmi.netlify.app/images/preview.jpg',
+  'og:image:width': '1190',
+  'og:image:height': '750',
 };
