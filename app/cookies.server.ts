c061b10d9d8d45ad8e8ad13b0af9cdb27f685a57
redasalmi <reda.salmi.elt@gmail.com>
2022-03-18 21:01:09 +0100

    added cookie secret

--- app/cookies.server.ts
+++ app/cookies.server.ts
@@ -1,12 +1,18 @@
 import { createCookie, json } from 'remix';
+import invariant from 'tiny-invariant';
+
 import type { Theme, ThemeData } from '~/types';
+
+const COOKIE_SECRET = process.env.COOKIE_SECRET;
+invariant(COOKIE_SECRET, 'COOKIE_SECRET must be set');
 
 const userTheme = createCookie('theme', {
   path: '/',
   sameSite: 'lax',
   httpOnly: true,
-  secure: true,
+  secrets: [COOKIE_SECRET],
   expires: new Date('2092-01-01'),
+  secure: process.env.NODE_ENV === 'production',
 });
 
 export const getuserTheme = async (headers: Headers) => {
