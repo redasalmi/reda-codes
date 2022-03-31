34e829b94ac4ed92f62ed10d80ecf53a84f150cf
redasalmi <reda.salmi.elt@gmail.com>
2022-03-31 21:16:24 +0100

    updated dependencies + replaced nav logo

--- app/components/Navbar/Logo.tsx
+++ app/components/Navbar/Logo.tsx
@@ -1,12 +1,10 @@
 import { Link } from 'remix';
 
-import { LogoSmall } from '~/components/icons';
-
 export default function Logo() {
   return (
     <div className="nav-logo">
       <Link to="/" className="nav-logo-link nav-link" aria-label="home page">
-        <LogoSmall width={150} height={49} className="nav-logo-svg" />
+        Reda Codes
       </Link>
     </div>
   );
