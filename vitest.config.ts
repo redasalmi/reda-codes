8f3bab2d1045bb1f77fe967c30e2dc64d6186af4
redasalmi <reda.salmi.elt@gmail.com>
2022-07-13 09:39:34 +0100

    switched to vitest

---
+++ vitest.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    setupFiles: ['dotenv/config', './test/setup-test-env.ts'],
+    include: ['./app/**/*.{test,spec}.{js,ts,jsx,tsx}'],
+    watchExclude: ['.*\\/node_modules\\/.*', '.*\\/build\\/.*'],
+  },
+});
