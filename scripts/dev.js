d9169ee36b619c3c57dccdda59a1dd1e7a9ee77c
redasalmi <reda.salmi.elt@gmail.com>
2022-10-12 00:38:53 +0100

    cleaned package.json scripts

--- scripts/dev.js
+++
@@ -1,6 +0,0 @@
-const runConcurrentlyScript = require('./runConcurrentlyScript');
-
-runConcurrentlyScript([
-  { command: 'css:build:watch', name: 'css' },
-  { command: 'dev:netlify', name: 'dev-server' },
-]);
--- scripts/validate.js
+++
@@ -1,9 +0,0 @@
-const runConcurrentlyScript = require('./runConcurrentlyScript');
-
-runConcurrentlyScript([
-  { command: 'lint', name: 'lint' },
-  { command: 'stylelint', name: 'stylelint' },
-  { command: 'typecheck', name: 'typecheck' },
-  { command: 'test:ci', name: 'test' },
-  { command: 'build:remix', name: 'build' },
-]);
