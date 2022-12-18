4cc7f2543d070bb3c8a24cd195cc295356bff556
redasalmi <reda.salmi.elt@gmail.com>
2022-12-18 21:42:22 +0100

    dependencies upgrade

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,6 @@
 const ERROR = 2;
 
+/** @type {import('eslint').Linter.Config} */
 module.exports = {
   extends: [
     '@remix-run/eslint-config',
@@ -9,6 +10,7 @@ module.exports = {
   rules: {
     'prefer-const': ERROR,
     'react/prop-types': ERROR,
+    'react/display-name': ERROR,
     '@typescript-eslint/no-unused-vars': ERROR,
   },
   ignorePatterns: [
