d227be55fde10e551fde9b52acf7b09e30113213
redasalmi <reda.salmi.elt@gmail.com>
2022-12-18 21:57:16 +0100

    entry.client update

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,22 +1,14 @@
+import * as React from 'react';
 import { hydrateRoot } from 'react-dom/client';
-import { startTransition, StrictMode } from 'react';
 import { RemixBrowser } from '@remix-run/react';
 
-function hydrate() {
-  startTransition(() => {
+setTimeout(() => {
+  React.startTransition(() => {
     hydrateRoot(
       document,
-      <StrictMode>
+      <React.StrictMode>
         <RemixBrowser />
-      </StrictMode>,
+      </React.StrictMode>,
     );
   });
-}
-
-if (window.requestIdleCallback) {
-  window.requestIdleCallback(hydrate);
-} else {
-  // Safari doesn't support requestIdleCallback
-  // https://caniuse.com/requestidlecallback
-  window.setTimeout(hydrate, 1);
-}
+}, 10);
