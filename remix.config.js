5e6b047b413334c2a31e6ce6f9ce95f477d8456e
redasalmi <reda.salmi.elt@gmail.com>
2023-04-02 23:23:53 +0100

    upgraded to remix 1.15.0

--- remix.config.js
+++ remix.config.js
@@ -6,6 +6,9 @@ module.exports = {
 		unstable_tailwind: true,
 		unstable_postcss: true,
 		v2_routeConvention: true,
+		v2_errorBoundary: true,
+		v2_normalizeFormMethod: true,
+		v2_meta: true,
 	},
 	ignoredRouteFiles: ['**/.*', '**/*.css', '**/*.test.{js,jsx,ts,tsx}'],
 	...(process.env.NODE_ENV === 'production'
