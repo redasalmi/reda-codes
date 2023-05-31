774db8e683884510155d8fbce31b24d510c52b25
redasalmi <reda.salmi.elt@gmail.com>
2023-05-31 19:39:01 +0100

    deleted all tsx svgs

--- app/components/Icons/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as LogoBig } from './LogoBig';
-export { default as LogoSmall } from './LogoSmall';
-export * from './types';
--- app/components/Icons/types.tsx
+++
@@ -1,5 +0,0 @@
-import type { Ref } from 'react';
-
-export type SvgPropsWithoutRef = React.ComponentPropsWithoutRef<'svg'>;
-export type SvgPropsWithRef = React.ComponentPropsWithRef<'svg'>;
-export type SvgRef = Ref<SVGSVGElement>;
