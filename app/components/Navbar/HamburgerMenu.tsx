79a3af06a80f9a463bf62e952fc99c254d1b57fe
redasalmi <reda.salmi.elt@gmail.com>
2023-06-04 15:40:38 +0100

    fixed handleToggleNavbar function logic

--- app/components/Navbar/HamburgerMenu.tsx
+++ app/components/Navbar/HamburgerMenu.tsx
@@ -44,11 +44,19 @@ export default function HamburgerMenu({
 		[controls],
 	);
 
+	const handleToggleNavbar = React.useCallback(
+		(toggleNavbar?: boolean) => {
+			const toggleIsOpen = toggleNavbar || !isOpen;
+			setIsOpen(toggleIsOpen);
+			updateNavbarAttributes(toggleIsOpen);
+		},
+		[isOpen, setIsOpen, updateNavbarAttributes],
+	);
+
 	React.useEffect(() => {
 		const handleEscapeKey = (event: KeyboardEvent) => {
 			if (event.key.toLowerCase() === 'escape') {
-				setIsOpen(false);
-				updateNavbarAttributes(false);
+				handleToggleNavbar(false);
 			}
 		};
 
@@ -57,20 +65,13 @@ export default function HamburgerMenu({
 		return () => {
 			window.removeEventListener('keydown', handleEscapeKey);
 		};
-	}, [setIsOpen, updateNavbarAttributes]);
+	}, [setIsOpen, handleToggleNavbar]);
 
-	const handleToggleNavbar = () => {
-		const toggleIsOpen = !isOpen;
-		setIsOpen(toggleIsOpen);
-		updateNavbarAttributes(toggleIsOpen);
-	};
-
-	const closeNavbar = (event: React.MouseEvent) => {
+	const handleClickOutSide = (event: React.MouseEvent) => {
 		const clickedOutsideNavbar = linksRef.current === event.target;
 
 		if (isOpen && clickedOutsideNavbar) {
-			setIsOpen(false);
-			updateNavbarAttributes(false);
+			handleToggleNavbar(false);
 		}
 	};
 
@@ -82,7 +83,7 @@ export default function HamburgerMenu({
 				ref={hamburgerBtnRef}
 				aria-label="open navigation menu"
 				className="m-auto block h-[45px] w-[45px] cursor-pointer rounded-full border-2 border-fg-black dark:border-fg-white"
-				onClick={handleToggleNavbar}
+				onClick={() => handleToggleNavbar()}
 				aria-expanded={false}
 				aria-controls="mobile-nav-menu"
 			>
@@ -102,7 +103,7 @@ export default function HamburgerMenu({
 				initial="init"
 				animate={controls}
 				id="mobile-nav-menu"
-				onClick={closeNavbar}
+				onClick={handleClickOutSide}
 				variants={linksVariants}
 				className="pointer-events-none fixed inset-0 z-[-1] hidden justify-end overflow-hidden md:hidden"
 			>
@@ -111,13 +112,13 @@ export default function HamburgerMenu({
 					className="flex h-full w-full translate-x-[110%] list-none flex-col bg-ghost-white pt-[90px] text-center dark:bg-bg-section-black sm:w-[320px] sm:text-left sm:shadow-[0_1px_3px] sm:shadow-[#4c4c4c]"
 				>
 					<Links
-						onClick={handleToggleNavbar}
+						onClick={() => handleToggleNavbar()}
 						linkClassName="text-[1.1rem] font-bold block py-5 border-t border-[#4c4c4c] hover:text-primary hover:dark:text-primary-dark sm:px-[54px] sm:text-[1.1rem]"
 					/>
 
 					<li className="border-t border-[#4c4c4c] py-5 md:hidden">
 						<GithubLink
-							onClick={handleToggleNavbar}
+							onClick={() => handleToggleNavbar()}
 							linkClassName="m-auto w-[70%] text-[1.2rem] rounded-3xl py-3 px-1 flex items-center justify-center text-fg-black dark:text-fg-white font-bold border-2 border-slate-blue sm:text-base sm:w-[80%]"
 							svgClassName="w-[25px] h-[25px] ml-[10px]"
 						>
