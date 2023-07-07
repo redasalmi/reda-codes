59fa54fd2b7e524851e16079e4b55fae70dafccf
redasalmi <reda.salmi.elt@gmail.com>
2023-07-07 20:15:34 +0100

    added css nesting and updated tailwind css

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,11 @@
-const tailwindcss = require('tailwindcss');
-const autoprefixer = require('autoprefixer');
-const postcssImport = require('postcss-import');
-const postcssPresetEnv = require('postcss-preset-env');
-const cssnano = require('cssnano');
-
-module.exports = (ctx) => {
-	const isProduction = ctx.env === 'production';
-	const plugins = [tailwindcss, autoprefixer, postcssImport, postcssPresetEnv];
-
-	if (isProduction) {
-		plugins.push(cssnano);
-	}
-
-	return {
-		plugins,
-	};
+/** @type {import('postcss-load-config').Config} */
+module.exports = {
+	plugins: [
+		require('postcss-import'),
+		require('@tailwindcss/nesting'),
+		require('tailwindcss'),
+		require('autoprefixer'),
+		require('postcss-preset-env'),
+		require('cssnano'),
+	],
 };
