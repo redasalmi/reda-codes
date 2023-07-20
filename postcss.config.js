60b236d3b20be6cfcc19224494e649a281b15620
redasalmi <reda.salmi.elt@gmail.com>
2023-07-20 23:57:01 +0100

    updated prettier and postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
 /** @type {import('postcss-load-config').Config} */
 module.exports = {
 	plugins: [
-		require('postcss-import'),
 		require('@tailwindcss/nesting'),
 		require('tailwindcss'),
 		require('autoprefixer'),
