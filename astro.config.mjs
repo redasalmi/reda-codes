338d4990f631b105617f97c2f88b010846819667
Reda Salmi <reda.salmi.elt@gmail.com>
2026-07-20 22:18:47 +0100

    adds navbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,30 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  fonts: [
+    {
+      provider: fontProviders.google(),
+      name: "IBM Plex Mono",
+      cssVariable: "--font-ibm-plex-mono",
+      weights: [400, 500, 600],
+      styles: ["normal"],
+      subsets: ["latin"],
+      fallbacks: ["monospace"],
+    },
+    {
+      provider: fontProviders.google(),
+      name: "Space Mono",
+      cssVariable: "--font-space-mono",
+      weights: [400, 700],
+      styles: ["normal"],
+      subsets: ["latin"],
+      fallbacks: ["monospace"],
+    },
+  ],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
