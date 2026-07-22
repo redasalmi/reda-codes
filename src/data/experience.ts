cd03a43aa5797340b61b173e6c1387f4b7970afe
Reda Salmi <reda.salmi.elt@gmail.com>
2026-07-22 21:41:13 +0100

    adds experience section

---
+++ src/data/experience.ts
@@ -0,0 +1,23 @@
+type Experience = {
+  title: string;
+  company: string;
+  date: string;
+};
+
+export const experiences: Experience[] = [
+  {
+    title: "Software Engineer",
+    company: "The Codest",
+    date: "2022—NOW",
+  },
+  {
+    title: "Full Stack Developer",
+    company: "ImaginePartners",
+    date: "2020—2021",
+  },
+  {
+    title: "Full Stack Developer",
+    company: "EmploiPartner",
+    date: "2019—2020",
+  },
+];
